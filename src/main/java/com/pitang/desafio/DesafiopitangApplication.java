4cd74fcec02307a45fb5b611c538724826b3c0b2
artvinicius <arthurviny1999@gmail.com>
2020-10-12 15:55:34 -0300

    Adicione Projeto

---
+++ src/main/java/com/pitang/desafio/DesafiopitangApplication.java
@@ -0,0 +1,13 @@
+package com.pitang.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiopitangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiopitangApplication.class, args);
+	}
+
+}
